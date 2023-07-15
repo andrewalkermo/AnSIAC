dc14ab2f70d151a788e8887fa33a1fd1ceb69001
Andre Walker <andre.walkermo@gmail.com>
2023-07-15 16:57:08 -0300

    merge_enabled

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dynaconf import Dynaconf
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
     settings_files=['settings.toml', '.secrets.toml'],
+    merge_enabled=True,
 )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
