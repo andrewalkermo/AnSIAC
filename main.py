0b652d26e24bb17661794e2bca9eebbfac13e5e9
Andre Walker <andre.walkermo@gmail.com>
2023-07-15 18:01:29 -0300

    refactor: organiza métodos

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import requests
 import jwt
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
 from config import settings
 
 USERNAME = settings.get('siac.username')
@@ -13,33 +13,32 @@ SECRET = settings.get('jwt.secret')
 def login() -> requests.Response:
 
   headers = {
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-      'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Origin': 'https://siac.ufba.br',
-      'Pragma': 'no-cache',
-      'Referer': 'https://siac.ufba.br/SiacWWW/Logoff.do',
-      'Sec-Fetch-Dest': 'document',
-      'Sec-Fetch-Mode': 'navigate',
-      'Sec-Fetch-Site': 'same-origin',
-      'Sec-Fetch-User': '?1',
-      'Upgrade-Insecure-Requests': '1',
-      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-      'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"Linux"'
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Origin': 'https://siac.ufba.br',
+    'Pragma': 'no-cache',
+    'Referer': 'https://siac.ufba.br/SiacWWW/Logoff.do',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"'
   }
 
   data = {
-      'cpf': USERNAME,
-      'senha': PASSWORD,
-      'x': '28',
-      'y': '11'
+    'cpf': USERNAME,
+    'senha': PASSWORD,
+    'x': '28',
+    'y': '11'
   }
 
-  # Perform the login request
   login_url = 'https://siac.ufba.br/SiacWWW/LogonSubmit.do'
   response = requests.post(login_url, headers=headers, data=data)
   if response.status_code == 200:
@@ -50,50 +49,45 @@ def login() -> requests.Response:
 def get_classes():
   response = login()
 
-  # Successful login, access the desired link
   coef_url = 'https://siac.ufba.br/SiacWWW/ConsultarComponentesCurricularesCursados.do'
   response = requests.get(coef_url, cookies=response.cookies)
   semester = get_semester()
+  semester_classes = []
 
-  if response.status_code == 200:
-      # Successful access to the desired link
-      soup = BeautifulSoup(response.content, 'html.parser')
+  if response.status_code != 200:
+    raise Exception('Failed to get classes')
 
-      table = soup.find('table', {'class': 'corpoHistorico'})
-      
-      tr_elements = table.find_all('tr')
+  soup = BeautifulSoup(response.content, 'html.parser')
+  table = soup.find('table', {'class': 'corpoHistorico'})
+  tr_elements = table.find_all('tr')
+  found = False
 
-      found = False
+  if not found:
+    for index, tr in enumerate(tr_elements):
       if not found:
-        for index, tr in enumerate(tr_elements):
-            if not found:
-              td_elements = tr.find_all('td')
-              for td in td_elements:
-                if td.b and td.b.text.strip() == semester:
-                    tr_elements = tr_elements[index:len(tr_elements)-2]
-                    found = True
-                    break
-      
-      if not found:
-        raise Exception('Semester not found')
+        td_elements = tr.find_all('td')
+        for td in td_elements:
+          if td.b and td.b.text.strip() == semester:
+            tr_elements = tr_elements[index:len(tr_elements)-2]
+            found = True
+            break
+  if not found:
+    raise Exception('Semester not found')
 
-      # Iterate through the td elements to find the one containing the desired semester
-      semester_classes = []
+  for index, tr in enumerate(tr_elements):
+    td_elements = tr.find_all('td')
 
-      for index, tr in enumerate(tr_elements):
-          td_elements = tr.find_all('td')
-          
-          name = td_elements[2].text.strip()
-          grade = td_elements[6].text.strip()
-          result = td_elements[7].text.strip()
+    name = td_elements[2].text.strip()
+    grade = td_elements[6].text.strip()
+    result = td_elements[7].text.strip()
 
-          semester_classes.append({
-              'name': name,
-              'grade': grade,
-              'result': result
-          })
-      
-      return semester_classes
+    semester_classes.append({
+      'name': name,
+      'grade': grade,
+      'result': result
+    })
+
+  return semester_classes
 
 def verify_grades():
   semester_classes = get_classes()
@@ -104,11 +98,11 @@ def verify_grades():
       token = file.read()
       last_grades = jwt.decode(token, SECRET, algorithms=['HS256'])['grades']
   except:
-     pass
+    pass
 
   for semester_class, last_grade in zip(semester_classes, last_grades):
     if semester_class['grade'] != '--':
-      print(f"{semester_class['name']} - {semester_class['grade']} - {semester_class['result']}")
+      # print(f"{semester_class['name']} - {semester_class['grade']} - {semester_class['result']}")
       if semester_class['grade'] != last_grade['grade']:
         print('New grade found!')
         send_telegram_message(f"Nova nota: {semester_class['name']}: {semester_class['grade']} - {semester_class['result']}")
@@ -119,39 +113,34 @@ def verify_grades():
     file.write(token)
 
 def send_telegram_message(message):
-    base_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": message,
-    }
-    
-    requests.post(base_url, data=payload)
-    
+  base_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+  payload = {
+    "chat_id": CHAT_ID,
+    "text": message,
+  }
 
+  requests.post(base_url, data=payload)
 
 def get_semester():
-    # Get the current date
-    current_date = datetime.now()
+  current_date = datetime.now()
 
-    # Determine the year and the semester
-    year = current_date.year
-    semester = None
+  year = current_date.year
+  semester = None
 
-    # Define the start and end dates for each semester (modify these as needed)
-    spring_start = datetime(year, 1, 1)
-    spring_end = datetime(year, 6, 30)
-    fall_start = datetime(year, 7, 1)
-    fall_end = datetime(year, 12, 31)
+  spring_start = datetime(year, 1, 1)
+  spring_end = datetime(year, 6, 30)
+  fall_start = datetime(year, 7, 1)
+  fall_end = datetime(year, 12, 31)
 
-    if spring_start <= current_date <= spring_end:
-        semester = "2"
-    elif fall_start <= current_date <= fall_end:
-        semester = "1"
+  if spring_start <= current_date <= spring_end:
+    semester = "2"
+  elif fall_start <= current_date <= fall_end:
+    semester = "1"
 
-    if semester:
-        return f"{year}.{semester}"
-    else:
-        return None
+  if semester:
+    return f"{year}.{semester}"
+  else:
+    return None
 
 if __name__ == '__main__':
   verify_grades()
